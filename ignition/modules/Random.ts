6d43ebbea8f6c5242f1a52be9ec368f464de666d
gibsfinancedev <162373480+gibsfinancedev@users.noreply.github.com>
2024-08-13 15:54:54 -0500

    Removes ignition files

--- ignition/modules/Random.ts
+++
@@ -1,9 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules"
-
-const RandomModule = buildModule("RandomModule", (m) => {
-  const random = m.contract('Random')
-
-  return { random }
-})
-
-export default RandomModule
