a14bf2443aa0e306c2b1c2e24efc15efac635226
gibsfinancedev <162373480+gibsfinancedev@users.noreply.github.com>
2024-09-05 22:46:51 -0500

    Removes gen step

--- packages/provider/codegen.config.cjs
+++ packages/provider/codegen.config.cjs
@@ -1,10 +1,3 @@
-fetch(process.env.INDEXER_URL)
-  .then((r) => {
-    if (r.ok) return r.text().then((res) => console.log(res))
-    else {
-      console.log(r)
-    }
-  })
 module.exports = {
   // Specify the schema URL
   schema: process.env.INDEXER_URL,
