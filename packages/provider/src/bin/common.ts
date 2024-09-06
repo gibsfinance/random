fc5a5125f2c7f710896566e2ae86d558aef1279e
gibsfinancedev <162373480+gibsfinancedev@users.noreply.github.com>
2024-09-05 22:54:51 -0500

    Adds event loop

--- packages/provider/src/bin/common.ts
+++ packages/provider/src/bin/common.ts
@@ -1,8 +1,22 @@
 import { db } from '../db'
 
+let EXITCONDITION = false
+
+const start = () => {
+  function wait() {
+    if (!EXITCONDITION)
+      setTimeout(wait, 1000)
+  }
+  wait()
+}
+
 export const main = (run: () => Promise<void>) => (
   db.migrate.latest()
+    .then(start)
     .then(() => run())
     .catch(console.error)
+    .then(() => {
+      EXITCONDITION = true
+    })
     .then(() => db.destroy())
 )
